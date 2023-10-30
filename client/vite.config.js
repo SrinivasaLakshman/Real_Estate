c9594b841efc1b524d30a679620d92f12ff13ab8
SrinivasaLakshman <srinivas.1718ccd@gmail.com>
2023-10-30 14:54:55 +0530

    creat sign up functionality

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import { nodePolyfills } from 'vite-plugin-node-polyfills';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        secure: false,
+      },
+    },
+  },
+
+  plugins: [
+    react(),
+    nodePolyfills(),
+  ],
+});
